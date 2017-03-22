af0adcd424e963e9b7d530770969af0be99b285d
jonetpranowo <jonet.pranowo@gmail.com>
2017-03-22 13:49:06 -0400

    Alternative HW using "dplyr" package

---
+++ Arrest.R
@@ -0,0 +1,26 @@
+#Read data
+dat <- read.csv("ArrestMini.csv")
+names(dat)
+
+#Create new variables: "DATE" and "TIME" from variables "ARRESTTIME"
+library(tidyr)
+library(dplyr)
+dat1 <- dat %>% 
+  separate(ARRESTTIME, c('DATE', 'TIME'), sep = 'T')
+
+#Aggregate by case
+Arrest_Daily <- dat1 %>%
+  mutate(Date = as.Date(DATE, "%m/%d/%Y")) %>%
+  group_by(DATE) %>%
+  summarize(CASES = n(), NUM_COUNCIL = n_distinct(COUNCIL_DISTRICT)) %>%
+  arrange(DATE) 
+
+
+View(Arrest_Daily)
+
+#Plot Date ~ Numb_Council
+RDATE <- as.Date(Arrest_Daily$DATE)
+fix(RDATE)
+plot(RDATE, Arrest_Daily$NUM_COUNCIL, type="l", col="red", xlab = "TIME", main = "Number of council districts that is affected by crime", ylab = "Number of Council")
+
+
